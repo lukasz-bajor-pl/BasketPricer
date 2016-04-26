67f98c55c870d7e35870febd0ac9cd44a2b86fa1
Lukasz Bajor <lukaszic@o2.pl>
2016-04-27 01:29:34 +0200

    Failing feature test for basketPricer

---
+++ src/test/java/bajor/lukasz/basketpricer/BasketPricerFeatureTest.java
@@ -0,0 +1,22 @@
+package bajor.lukasz.basketpricer;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by lbajor on 2016-04-27.
+ */
+public class BasketPricerFeatureTest {
+    ItemPricer pricer = new SimpleItemPricer();
+    BasketPricer basketPricer = new SimpleBasketPricer(pricer);
+
+    @Test
+    public void shouldCalculateIndividualPricesOfAllItemsInBasket() {
+        Basket b = new BasketImpl(Item.Apple, Item.Apple, Item.Banana);
+
+        assertEquals(Item.Apple.price.multiply(new BigDecimal(2)).add(Item.Banana.price), basketPricer.getPrice(b));
+    }
+}
