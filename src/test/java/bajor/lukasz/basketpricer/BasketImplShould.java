4b538d904319723791685dad120897a1d77cdf3f
Lukasz Bajor <lukaszic@o2.pl>
2016-04-27 00:29:13 +0200

    Failing test for single item basket / Passing test to get items from single element basket - refactor

--- src/test/java/bajor/lukasz/basketpricer/BasketImplShould.java
+++ src/test/java/bajor/lukasz/basketpricer/BasketImplShould.java
@@ -1,12 +1,12 @@
 package bajor.lukasz.basketpricer;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import java.util.Map;
 
 import static bajor.lukasz.basketpricer.Item.Apple;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasSize;
 
 
@@ -23,6 +23,6 @@ public class BasketImplShould {
     public void returnOneItemFromOneElementBasket() {
         Map<Item, Long> items = new BasketImpl(Apple).getItems();
         assertThat(items.keySet(), hasSize(1));
-        assertThat(items, Matchers.hasEntry(Apple, 1L));
+        assertThat(items, hasEntry(Apple, 1L));
     }
 }
