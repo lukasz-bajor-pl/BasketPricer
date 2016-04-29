57708a510406440569b34e580f29d27a72b67be9
Lukasz Bajor <lukaszic@o2.pl>
2016-04-30 00:38:02 +0200

    Failing feature test for basket pricer - promotions not calculated correctly / passing offers pricer decoration unit test when offer is present - refactor

--- src/test/java/bajor/lukasz/basketpricer/offer/OffersItemPricerDecoratorShould.java
+++ src/test/java/bajor/lukasz/basketpricer/offer/OffersItemPricerDecoratorShould.java
@@ -1,19 +1,20 @@
 package bajor.lukasz.basketpricer.offer;
 
-import bajor.lukasz.basketpricer.Item;
 import bajor.lukasz.basketpricer.ItemPricer;
 import bajor.lukasz.basketpricer.SimpleItemPricer;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashSet;
+
+import static bajor.lukasz.basketpricer.Item.Apple;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by lbajor on 2016-04-30.
@@ -28,19 +29,19 @@ public class OffersItemPricerDecoratorShould {
     @Test
     public void callSubPricerForPriceCalculation() {
         withOffersPricer = new OffersItemPricerDecorator(Collections.emptySet(), subPricerMock);
-        withOffersPricer.getPrice(Item.Apple, 3);
+        withOffersPricer.getPrice(Apple, 3);
 
-        Mockito.verify(subPricerMock).getPrice(Item.Apple, 3);
+        verify(subPricerMock).getPrice(Apple, 3);
     }
 
     @Test
     public void applyOfferForSameItem() {
         withOffersPricer = new OffersItemPricerDecorator(new HashSet<Offer>() {
             {
-                add(new BulkBuyOffer(Item.Apple, 2, 1));
+                add(new BulkBuyOffer(Apple, 2, 1));
             }
         }, new SimpleItemPricer());
 
-        Assert.assertThat(withOffersPricer.getPrice(Item.Apple, 3), CoreMatchers.equalTo(Item.Apple.price.multiply(new BigDecimal(2))));
+        assertThat(withOffersPricer.getPrice(Apple, 3), equalTo(Apple.price.multiply(new BigDecimal(2))));
     }
 }
